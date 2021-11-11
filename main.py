91f0ac55f5b8dbf05a868865c61174d482acf2e6
diala-eng <71458903+diala-eng@users.noreply.github.com>
2021-11-11 15:51:12 +0200

    The search topic and id in front end server

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+import requests
 
 app = Flask(__name__)
+
+
+@app.route('/searchTopic/<string:topic>', methods=['GET'])
+def searchByBookTopic(topic):
+    req=requests.get("http://192.168.100.8:7000/search/{}".format(topic)) #send to catalog
+
+    if req.status_code == 200:
+
+        result=req.json() #content of json as dictionary
+
+        return jsonify(result)
+
+    elif req.status_code == 404:
+        return 'The server has not found anything matching the URI given'
+
+    else:
+        return 'Status code '+ req.status_code +' indicates to something ERROR!'
+
+
+@app.route('/searchID/<int:id>', methods=['GET'])
+def searchByBookId(id):
+    req=requests.get("http://192.168.100.8:7000/info/{}".format(id))
+
+    if req.status_code == 200:
+        result=req.json() #content of json as dictionary
+
+        return jsonify(result)
+
+    elif req.status_code == 404:
+        return 'The server has not found anything matching the URI given'
+
+    else:
+        return 'Status code '+ req.status_code +' indicates to something ERROR!'
 
 
 
