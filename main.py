d84908ec66194af5ba8334b339774bdd6e9d82bf
waedkhalifa <62521979+waedkhalifa@users.noreply.github.com>
2021-11-13 22:50:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,28 @@ from flask import Flask, jsonify
 
 import requests
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 @app.route('/searchTopic/<string:topic>', methods=['GET'])
 def searchByBookTopic(topic):
-    req=requests.get("http://192.168.100.8:7000/search/{}".format(topic)) #send to catalog
+    req=requests.get("http://192.168.56.101:7000/search/{}".format(topic)) #send to catalog
 
     if req.status_code == 200:
-
         result=req.json() #content of json as dictionary
 
         return jsonify(result)
 
     elif req.status_code == 404:
-        return 'The server has not found anything matching the given URI',404
+        return 'The server has not found anything matching the given URL',404
 
     else:
-        return 'Status code indicates to something ERROR!',req.status_code
+        return 'Status code '+ str(req.status_code) +' indicates to something ERROR!',req.status_code
 
 
 @app.route('/searchID/<int:id>', methods=['GET'])
 def searchByBookId(id):
-    req=requests.get("http://192.168.100.8:7000/info/{}".format(id))
+    req=requests.get("http://192.168.56.101:7000/info/{}".format(id))
 
     if req.status_code == 200:
         result=req.json() #content of json as dictionary
@@ -32,26 +31,26 @@ def searchByBookId(id):
         return jsonify(result)
 
     elif req.status_code == 404:
-        return 'The server has not found anything matching the given URI',404
+        return 'The server has not found anything matching the given URL',404
 
     else:
-        return 'Status code indicates to something ERROR!',req.status_code
+        return 'Status code '+ str(req.status_code) +' indicates to something ERROR!',req.status_code
 
 @app.route('/purchase/<int:id>', methods=['POST'])
 def purchase(id):
 
-    req = requests.post("http://192.168.100.9:9000/purchase/{}".format(id))
+    req = requests.get("http://192.168.65.103:9000/updateinfo/{}".format(id))
 
     if req.status_code == 200:
         result = req.json()  # content of json as dictionary
         return jsonify(result)
 
     elif req.status_code == 404:
-        return 'The server has not found anything matching the given URI',404
+        return 'The server has not found anything matching the URI given',404
 
     else:
-        return 'Status code indicates to something ERROR!',req.status_code
+        return 'Status code '+ str(req.status_code) +' indicates to something ERROR!',req.status_code
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=6000)
